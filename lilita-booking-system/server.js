22cebbd89bd60dac0bd108b5cfa21d7aa8da6f7d
sparksnairobi-hub <sparksnairobi@gmail.com>
2026-08-23 10:10:07 +0300

    Fix response interception middleware for API URL rewriting

--- lilita-booking-system/server.js
+++ lilita-booking-system/server.js
@@ -24,16 +24,18 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-// Custom middleware to fix API URL in JavaScript files
-app.get('*.js', (req, res, next) => {
-  const filePath = path.join(__dirname, 'frontend/dist', req.path);
-  try {
-    let content = fs.readFileSync(filePath, 'utf8');
-    content = content.replace(/http:\/\/localhost:3002/g, '/api');
-    res.type('application/javascript').send(content);
-  } catch (e) {
-    next();
+// Middleware to intercept and rewrite JavaScript responses
+app.use((req, res, next) => {
+  if (req.path.match(/\.js$/)) {
+    const originalSend = res.send.bind(res);
+    res.send = function(data) {
+      if (typeof data === 'string') {
+        data = data.replace(/http:\/\/localhost:3002/g, '/api');
+      }
+      return originalSend(data);
+    };
   }
+  next();
 });
 
 // Serve React frontend from frontend/dist
